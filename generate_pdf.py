032aed03f54afb40960de2c281c231736e242010
Jean-Frederic Plante <jfrederic.plante@gmail.com>
2020-08-30 11:11:42 -0700

    layout adjusts

--- generate_pdf.py
+++ generate_pdf.py
@@ -4,9 +4,67 @@ import sys
 import random
 from random import randint
 import argparse
+from fpdf import FPDF
+import numpy as np
 
-def get_decoder_list(char_set):
-  max_op_res = 200
+
+class PDF(FPDF):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)    
+    self.length = 240
+    self.set_draw_color(0,0,0)
+
+  def write_operations(self, y =20, operations_dict={}):
+    colum_ht = int(len(operations_dict)/2)
+    self.set_font("Arial", size=16)
+    current_col = "left"
+    current_y = y
+    cell_length = 60
+    for c, o in operations_dict.items():
+      if current_col == "left":
+        current_x = 10
+        self.set_xy(current_x,current_y)
+        self.cell(cell_length,cell_length, o, border=0,align="C")
+        current_x += 40
+        self.set_xy(current_x,current_y)
+        self.cell(cell_length,cell_length, c, border=0,align="C")
+        current_col = "right"
+      else:
+        current_x = 120
+        self.set_xy(current_x,current_y)
+        self.cell(cell_length,cell_length, o, border=0,align="C")
+        current_x += 40
+        self.set_xy(current_x,current_y)
+        self.cell(cell_length,cell_length, c, border=0,align="C")
+        current_col = "left"
+        current_y += 10
+
+  def generate_writing_line(self, x=20, y=20, segments = 10, line_array=[], font_size=12):
+    self.set_font("Arial", size=font_size)
+    interval_between_lines = 20
+    line_y = y
+    line_dash_ratio = 2/3
+    distance_between_cells = (self.length-x) / segments 
+    cell_length = line_dash_ratio * distance_between_cells 
+    current_x = x
+    # c_to_write = np.resize(c_to_write, segments)
+    for w in line_array:
+      y_text_offset = +12
+      for c in w:
+        if c != " ":
+          self.set_xy(current_x,y)
+          self.cell(cell_length,cell_length, border="B",align="C")
+          self.set_xy(current_x,y+y_text_offset)
+          self.cell(cell_length,cell_length, txt=str(c), border=0,align="C")
+        current_x += distance_between_cells
+    return distance_between_cells
+
+  def add_title(self, title="exercise", font_size=25, y = 5):
+    self.set_xy(0,y)
+    self.set_font("Arial", size=font_size)
+    self.multi_cell(220, 10, txt=title, align="C", border=0)
+
+def get_decoder_list(char_set, max_op_res=300):
   num_list = []
   while len(num_list) <= len(char_set):
     r=random.randint(1,max_op_res)
@@ -68,7 +126,7 @@ def generate_operations(decoder_ring, ops=['multiply','add','divide','subtract']
       while (invalid_operation):
         a = random.randint(1,v)
         invalid_operation = (v*a > 1000) or (a == 1)
-      calc_dict[c] = str(f"{v*a} ÷ {a} = ")
+      calc_dict[c] = str(f"{v*a} / {a} = ")
     elif o == "add":
       a = random.randint(1,v)
       calc_dict[c] = str(f"{v-a} + {a} = ")
@@ -79,64 +137,7 @@ def generate_operations(decoder_ring, ops=['multiply','add','divide','subtract']
       print(f"error, op {o} not found")
   return calc_dict
 
-from fpdf import FPDF
-import numpy as np
- 
-class PDF(FPDF):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)    
-    self.length = 220
-    self.set_draw_color(0,0,0)
 
-  def write_operations(self, y =20, operations_dict={}):
-    colum_ht = int(len(operations_dict)/2)
-    self.set_font("Arial", size=16)
-    current_col = "left"
-    current_y = y
-    cell_length = 60
-    for c, o in operations_dict.items():
-      if current_col == "left":
-        current_x = 10
-        self.set_xy(current_x,current_y)
-        self.cell(cell_length,cell_length, o, border=0,align="C")
-        current_x += 40
-        self.set_xy(current_x,current_y)
-        self.cell(cell_length,cell_length, c, border=0,align="C")
-        current_col = "right"
-      else:
-        current_x = 120
-        self.set_xy(current_x,current_y)
-        self.cell(cell_length,cell_length, o, border=0,align="C")
-        current_x += 40
-        self.set_xy(current_x,current_y)
-        self.cell(cell_length,cell_length, c, border=0,align="C")
-        current_col = "left"
-        current_y += 10
-
-  def generate_writing_line(self, x=20, y=20, segments = 10, line_array=[], font_size=12):
-    self.set_font("Arial", size=font_size)
-    interval_between_lines = 20
-    line_y = y
-    line_dash_ratio = 2/3
-    distance_between_cells = (self.length-x) / segments 
-    cell_length = line_dash_ratio * distance_between_cells 
-    current_x = x
-    # c_to_write = np.resize(c_to_write, segments)
-    for w in line_array:
-      y_text_offset = +10
-      for c in w:
-        if c != " ":
-          self.set_xy(current_x,y)
-          self.cell(cell_length,cell_length, border="B",align="C")
-          self.set_xy(current_x,y+y_text_offset)
-          self.cell(cell_length,cell_length, txt=str(c), border=0,align="C")
-        current_x += distance_between_cells
-    return distance_between_cells
-
-  def add_title(self, title="exercise", font_size=25, y = 5):
-    self.set_xy(0,y)
-    self.set_font("Arial", size=font_size)
-    self.multi_cell(220, 10, txt=title, align="C", border=0)
 
 def gen_worksheet(y_content = 50, y_title=30, 
                   sheet_title="Break the code!", 
@@ -163,11 +164,11 @@ def gen_worksheet(y_content = 50, y_title=30,
   current_y = CONTENT_Y
   segments = max([sum([len(wc) for wc in cline]) for cline in code_array])
   
-  pdf.write_operations(y=10, operations_dict=operations_dict)
-  current_y = 120
+  pdf.write_operations(y=5, operations_dict=operations_dict)
+  current_y = 110
   for l in code_array:
     pdf.generate_writing_line(y=current_y, line_array=l, segments = segments)
-    current_y += 25
+    current_y += 23
 
   pdf.output(outputname)
   return 
@@ -181,8 +182,6 @@ def is_prime(num):
            return False
    else:
        return True
-
-
 
 def main():
     parser = argparse.ArgumentParser()
